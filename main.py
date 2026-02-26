f6c664c8b85b34fb0c59a21496f86edf4bff76e8
Jey Chhun <136158401+chanhengchhun@users.noreply.github.com>
2026-02-26 11:57:21 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+
+def dfs(node, visited_list=None):
+    if visited_list is None:
+        visited_list = []
+    # return if tree doesn't exist
+    if node is None:
+        return visited_list
+
+    visited_list.append(node.val)
+    dfs(node.left, visited_list) # 3 explore left branch
+    dfs(node.right, visited_list) # 4 explore right branch
+
+    return visited_list
+
+
+
